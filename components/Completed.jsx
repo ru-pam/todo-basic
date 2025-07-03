fbd47ad59249d024dd37169e377af191dd8e9fab
Rupam <rupamcodes@gmail.com>
2025-07-03 20:23:12 +0530

    added no of remaining and completed tasks

--- components/Completed.jsx
+++ components/Completed.jsx
@@ -10,40 +10,43 @@ const Completed = (props) => {
   return (
     <div>
       {completedList.length > 0 ? (
-        completedList.map((completed, index) => (
-          <li
-            key={index}
-            defaultChecked={true}
-            className={"flex gap-2 items-center justify-between"}
-          >
-            <input
-              type="checkbox"
-              checked={true}
-              onChange={() =>
-                handleChange(
-                  completedList,
-                  index,
-                  setCompletedList,
-                  taskList,
-                  setTaskList
-                )
-              }
-              className={"size-[20px]"}
-            />
-            {completed}
-            <button
-              onClick={() =>
-                handleDeleteTask(index, completedList, setCompletedList)
-              }
-              type={"button"}
-              className={
-                "bg-[#E3DE61] text-sm !p-1 rounded-lg cursor-pointer text-black"
-              }
+        <>
+          <h3 className={"!mb-4"}>Completed tasks: {completedList.length}</h3>
+          {completedList.map((completed, index) => (
+            <li
+              key={index}
+              defaultChecked={true}
+              className={"flex gap-2 items-center justify-between"}
             >
-              Delete
-            </button>
-          </li>
-        ))
+              <input
+                type="checkbox"
+                checked={true}
+                onChange={() =>
+                  handleChange(
+                    completedList,
+                    index,
+                    setCompletedList,
+                    taskList,
+                    setTaskList,
+                  )
+                }
+                className={"size-[20px]"}
+              />
+              {completed}
+              <button
+                onClick={() =>
+                  handleDeleteTask(index, completedList, setCompletedList)
+                }
+                type={"button"}
+                className={
+                  "bg-[#E3DE61] text-sm !p-1 rounded-lg cursor-pointer text-black"
+                }
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </>
       ) : (
         <div>{taskList.length > 0 && <h1>No completed tasks</h1>}</div>
       )}
--- components/Task.jsx
+++ components/Task.jsx
@@ -1,29 +1,32 @@
-//fix the bug in checkbox in completed task section (checkbox should be checked)
 function Task(props) {
-  const { taskList, setTaskList, handleDeleteTask, handleChange, completedList, setCompletedList, checked } = props;
+  const { taskList, setTaskList, handleDeleteTask, handleChange, completedList, setCompletedList } = props;
   return (
     <div>
       {taskList.length > 0 ? (
-        taskList.map((task, index) => (
-          <li
-            key={index}
-            className={"flex gap-2 items-center justify-between"}
-          >
-            <input type="checkbox"
-                   checked={false}
-                   onChange={() => handleChange(taskList, index, setTaskList, completedList, setCompletedList)} className={"size-[20px]"} />
-            {task}
-            <button
-              onClick={() => handleDeleteTask(index, taskList, setTaskList)}
-              type={"button"}
-              className={
-                "bg-[#E3DE61] text-sm !p-1 rounded-lg cursor-pointer text-black"
-              }
+        <>
+          <h3 className={"!mb-4"}>Remaining Tasks: {taskList.length}</h3>
+          {taskList.map((task, index) => (
+            <li
+              key={index}
+              className={"flex gap-2 items-center justify-between"}
             >
-              Delete
-            </button>
-          </li>
-        ))
+              <input type="checkbox"
+                     checked={false}
+                     onChange={() => handleChange(taskList, index, setTaskList, completedList, setCompletedList)}
+                     className={"size-[20px]"}/>
+              {task}
+              <button
+                onClick={() => handleDeleteTask(index, taskList, setTaskList)}
+                type={"button"}
+                className={
+                  "bg-[#E3DE61] text-sm !p-1 rounded-lg cursor-pointer text-black"
+                }
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </>
       ) : (
         <p className={"text-[#E3DE61] text-2xl text-center !mt-5 font-bold"}>
           Add tasks
